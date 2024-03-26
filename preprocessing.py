25b290d65adae968629b01c3f03229fc5c927d8b
madaodog <mohamed.azzarouali@student.kuleuven.be>
2024-03-26 15:41:21 +0100

    fixed datasets

--- preprocessing.py
+++ preprocessing.py
@@ -1,36 +1,31 @@
 import pandas as pd
-import math
-
-df = pd.read_parquet('0000.parquet')
-
-text_data = df
-tokens = text_data['tokens']
-tags = text_data['ner_tags']
-
-
-
-train_df_length = math.ceil(0.7 * len(df))
-dev_df_length = math.ceil(0.85 * len(df))
-test_df_length = len(df)
-
-tag_dict = {
-    0: 'O',
-    1: 'B-PER',
-    2: 'I-PER',
-    3: 'B-ORG',
-    4: 'I-ORG',
-    5: 'B-LOC',
-    6: 'I-LOC',
-}
 
 def write_to_tsv(df, filename):
+    tag_dict = {
+        0: 'O',
+        1: 'B-PER',
+        2: 'I-PER',
+        3: 'B-ORG',
+        4: 'I-ORG',
+        5: 'B-LOC',
+        6: 'I-LOC',
+    }
+
     with open(filename, 'w', encoding='utf-8') as f:
         for index, row in df.iterrows():
             for token, tag_index in zip(row['tokens'], row['ner_tags']):
                 tag_label = tag_dict[tag_index]
                 f.write(str(token) + '\t' + str(tag_label) + '\n')
-            f.write('\n')  
+            f.write('\n')
 
-write_to_tsv(df[:train_df_length], 'cbk_train.tsv')
-write_to_tsv(df[train_df_length:dev_df_length], 'cbk_dev.tsv')
-write_to_tsv(df[dev_df_length:test_df_length], 'cbk_test.tsv')
+# Process cbk_train.parquet
+train_df = pd.read_parquet('cbk_train.parquet')
+write_to_tsv(train_df, 'cbk_train.tsv')
+
+# Process cbk_validation.parquet
+validation_df = pd.read_parquet('cbk_validation.parquet')
+write_to_tsv(validation_df, 'cbk_validation.tsv')
+
+# Process cbk_test.parquet
+test_df = pd.read_parquet('cbk_test.parquet')
+write_to_tsv(test_df, 'cbk_test.tsv')
